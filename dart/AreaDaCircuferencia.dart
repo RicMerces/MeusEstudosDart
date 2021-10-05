bfac5b378572f93d4a83f1882b5e8693e5538137
ricardoliveiraof2m <ricardoliveira0703@gmail.com>
2021-10-04 21:18:25 -0300

    AreaDaCircuferencia pt01 + constantes

---
+++ dart/AreaDaCircuferencia.dart
@@ -0,0 +1,7 @@
+import 'dart:io';
+
+main() {
+  //Area da circuferencia = PI * (r * r)
+  String texto = stdin.readLineSync()!;
+  print(texto);
+}
--- dart/README.md
+++ dart/README.md
@@ -35,5 +35,5 @@ main(){
 }
 ```
 
-
+### Constantes
 
