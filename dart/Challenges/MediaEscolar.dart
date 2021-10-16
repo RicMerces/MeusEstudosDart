18dad940679cf3cdc647745614b951f82920bd45
ricardoliveiraof2m <ricardoliveira0703@gmail.com>
2021-10-16 15:47:23 -0300

    MediaEscolar

--- dart/Challenges/MediaEscolar.dart
+++ dart/Challenges/MediaEscolar.dart
@@ -1,11 +1,34 @@
 import 'dart:io';
 
 void main() {
-  var nome = stdin.realLineSync()!;
+  print("Informe seu nome");
+  var nome = stdin.readLineSync();
+  String aprovacao = "a calcular";
 
-  print("");
+  print(
+      "Informe suas notas respectivamente de cada Unidade (lembrando que são 3) ");
+  var valor01 = stdin.readLineSync();
+  var valor02 = stdin.readLineSync();
+  var valor03 = stdin.readLineSync();
+  double nota1 = double.parse(valor01!);
+  double nota2 = double.parse(valor02!);
+  double nota3 = double.parse(valor03!);
 
+  double media = (nota1 + nota2 + nota3) / 3;
+  var oAluno = "$nome sua Média Final foi : $media logo você foi ";
+  String precisando = "Precisando de ${7 - media} Pontos";
 
-
-
+  if (media >= 6) {
+    aprovacao = "APROVADO";
+    print(oAluno + aprovacao);
+  } else if (media < 6 && media >= 5) {
+    aprovacao = "PARA PROVA FINAL $precisando";
+    print(oAluno + aprovacao);
+  } else if (media < 5 && media >= 4) {
+    aprovacao = "PARA A RECUPERAÇÃO $precisando";
+    print(oAluno + aprovacao);
+  } else {
+    aprovacao = "REPROVADO pois estava $precisando";
+    print(oAluno + aprovacao);
+  }
 }
