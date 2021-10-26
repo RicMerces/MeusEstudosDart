3ad83979628b6b42a9ec7e862771e2d015d1a0d4
ricardoliveiraof2m <ricardoliveira0703@gmail.com>
2021-10-25 22:16:04 -0300

    Desafio16 (versão sem while))

--- dart/Challenges/Parte01/Desafio16.dart
+++ dart/Challenges/Parte01/Desafio16.dart
@@ -5,10 +5,13 @@ void main() {
   var area = stdin.readLineSync()!;
   double litro = (double.parse(area)) / 3;
   print("É necessario $litro Litros");
-  int lata = 1;
-  while (litro > 18) {
+  /*int lata = 1;
+   while (litro > 18) {
     litro - 18;
     lata++;
-  }
+  } */
+  double lata = litro / 18;
+
   print("Numero de latas = $lata");
+  double preco = lata * 80;
 }
