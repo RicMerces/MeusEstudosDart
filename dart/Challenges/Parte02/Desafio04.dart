ecb28e95dd3d13b4f73d087bd6b80ef454250186
ricardoliveiraof2m <ricardoliveira0703@gmail.com>
2021-10-26 23:50:40 -0300

    Desafio04 superpowerdrop

---
+++ dart/Challenges/Parte02/Desafio04.dart
@@ -0,0 +1,22 @@
+import 'dart:io';
+
+void main() {
+  print("Informe uma vogal ou consoante");
+  var letra = stdin.readLineSync()!;
+
+  if (letra.toUpperCase() == 'A' ||
+      letra.toUpperCase() == 'E' ||
+      letra.toUpperCase() == 'I' ||
+      letra.toUpperCase() == 'O' ||
+      letra.toUpperCase() == 'U') {
+    print("VOGAL");
+  } else if (double.parse(letra) % 2 == 0 || double.parse(letra) % 2 != 0) {
+    print("AMIGO ISSO É UM NUMERO");
+  } else if (letra.toUpperCase() != 'A' ||
+      letra.toUpperCase() != 'E' ||
+      letra.toUpperCase() != 'I' ||
+      letra.toUpperCase() != 'O' ||
+      letra.toUpperCase() != 'U') {
+    print("CONSOANTE");
+  }
+}
