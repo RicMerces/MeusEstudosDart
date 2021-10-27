2689e3f1aed3b1f0d4d00121d9e8fecd826407c2
ricardoliveiraof2m <ricardoliveira0703@gmail.com>
2021-10-27 07:46:14 -0300

    Correcao

--- dart/Challenges/Parte02/Desafio09.dart
+++ dart/Challenges/Parte02/Desafio09.dart
@@ -1,16 +1,26 @@
 import 'dart:io';
 
 void main() {
-  print("Informe seu turno");
-  String turno = stdin.readLineSync()!;
+  print("Informe três valores");
+  var n1 = stdin.readLineSync()!;
+  var n2 = stdin.readLineSync()!;
+  var n3 = stdin.readLineSync()!;
 
-  if (turno.toUpperCase() == 'MATUTINO') {
-    print("Bom Dia !");
-  } else if (turno.toUpperCase() == 'VESPERTINO') {
-    print("Boa Tarde !");
-  } else if (turno.toUpperCase() == 'NOTURNO') {
-    print("Boa Noite !");
+  double nu1 = double.parse(n1);
+  double nu2 = double.parse(n2);
+  double nu3 = double.parse(n3);
+
+  if (nu1 >= nu2 && nu1 > 3 && nu2 >= nu3) {
+    print("$nu1 > $nu2 > $nu3");
+  } else if (nu2 >= nu1 && nu2 > nu3 && nu1 >= nu3) {
+    print("$nu2 > $nu1 > $nu3");
+  } else if (nu3 >= nu1 && nu3 > nu2 && nu1 >= nu2) {
+    print("$nu3 > $nu1 > $nu2");
+  } else if (nu3 >= nu2 && nu3 > nu1 && nu2 >= nu1) {
+    print("$nu3 > $nu2 > $nu1");
+  } else if (nu2 >= nu3 && nu2 > nu1 && nu3 >= nu1) {
+    print("$nu2 > $nu3 > $nu1");
   } else {
-    print("Esse turno não existe");
+    print("$nu1 = $nu2 = $nu3");
   }
 }
--- dart/Challenges/Parte02/Desafio10.dart
+++ dart/Challenges/Parte02/Desafio10.dart
@@ -1,26 +1,16 @@
 import 'dart:io';
 
 void main() {
-  print("Informe três valores");
-  var n1 = stdin.readLineSync()!;
-  var n2 = stdin.readLineSync()!;
-  var n3 = stdin.readLineSync()!;
+  print("Informe seu turno");
+  String turno = stdin.readLineSync()!;
 
-  double nu1 = double.parse(n1);
-  double nu2 = double.parse(n2);
-  double nu3 = double.parse(n3);
-
-  if (nu1 >= nu2 && nu1 > 3 && nu2 >= nu3) {
-    print("$nu1 > $nu2 > $nu3");
-  } else if (nu2 >= nu1 && nu2 > nu3 && nu1 >= nu3) {
-    print("$nu2 > $nu1 > $nu3");
-  } else if (nu3 >= nu1 && nu3 > nu2 && nu1 >= nu2) {
-    print("$nu3 > $nu1 > $nu2");
-  } else if (nu3 >= nu2 && nu3 > nu1 && nu2 >= nu1) {
-    print("$nu3 > $nu2 > $nu1");
-  } else if (nu2 >= nu3 && nu2 > nu1 && nu3 >= nu1) {
-    print("$nu2 > $nu3 > $nu1");
+  if (turno.toUpperCase() == 'MATUTINO') {
+    print("Bom Dia !");
+  } else if (turno.toUpperCase() == 'VESPERTINO') {
+    print("Boa Tarde !");
+  } else if (turno.toUpperCase() == 'NOTURNO') {
+    print("Boa Noite !");
   } else {
-    print("$nu1 = $nu2 = $nu3");
+    print("Esse turno não existe");
   }
 }
