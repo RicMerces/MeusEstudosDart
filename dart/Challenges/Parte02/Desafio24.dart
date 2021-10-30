bbca5b605b8f50c44c5191dc803edcd5ecdb7bc7
ricardoliveiraof2m <ricardoliveira0703@gmail.com>
2021-10-30 20:23:41 -0300

    Desafio24

---
+++ dart/Challenges/Parte02/Desafio24.dart
@@ -0,0 +1,28 @@
+import 'dart:_internal' show printToConsole;
+import 'dart:io';
+
+void main0() {
+  print("Informe um numero");
+  var n = stdin.readLineSync()!;
+  double numero = double.parse(n);
+
+  if (numero % 2 == 0) {
+    print("Par");
+  } else {
+    print("Impar");
+  }
+
+  if (numero > 0) {
+    print(" Positivo");
+  } else if (numero < 0) {
+    print(" Negativo");
+  } else {
+    print(" Zero");
+  }
+
+  if (numero is double) {
+    print(" Decimal");
+  } else {
+    print(" Inteiro");
+  }
+}
