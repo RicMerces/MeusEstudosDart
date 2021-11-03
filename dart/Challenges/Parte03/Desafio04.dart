334932cd7a17a88f9ae348ba99f7971ce6378e69
ricardoliveiraof2m <ricardoliveira0703@gmail.com>
2021-11-03 20:41:38 -0300

    Desafio04

---
+++ dart/Challenges/Parte03/Desafio04.dart
@@ -0,0 +1,23 @@
+import 'dart:io';
+
+void mainn() {
+  double paisA = 80000;
+  double paisB = 200000;
+  int ano = 0;
+
+  double aumentoA = 0;
+  double aumentoB = 0;
+
+  while (paisA < paisB) {
+    aumentoA = paisA + (0.03 * paisA);
+    aumentoB = paisB + (0.015 * paisB);
+
+    paisA = aumentoA;
+    paisB = aumentoB;
+
+    ano++;
+  }
+
+  print(
+      "O tempo estimado para que a população da cidade A ultrapasse a cidade B é de $ano anos");
+}
