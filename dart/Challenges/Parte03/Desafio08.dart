6d92c0ece6597e9402e8585b1c38400d5a3294b0
ricardoliveiraof2m <ricardoliveira0703@gmail.com>
2021-11-04 01:00:04 -0300

    Desafio08

--- dart/Challenges/Parte03/Desafio08.dart
+++ dart/Challenges/Parte03/Desafio08.dart
@@ -2,7 +2,7 @@ import 'dart:io';
 
 void main() {
   var nA, nB, nC, nD, nE;
-  double n1, n2, n3, n4, n5;
+  var n1, n2, n3, n4, n5;
 
   for (int i = 0; i <= 5; i++) {
     if (i == 0) {
@@ -12,19 +12,20 @@ void main() {
     } else if (i == 1) {
       print("Informe o ${i + 1}º numero ");
       nB = stdin.readLineSync();
-      n2 = double.parse(nA);
+      n2 = double.parse(nB);
     } else if (i == 2) {
       print("Informe o ${i + 1}º numero ");
       nC = stdin.readLineSync();
-      n3 = double.parse(nA);
+      n3 = double.parse(nC);
     } else if (i == 3) {
       print("Informe o ${i + 1}º numero ");
       nD = stdin.readLineSync();
-      n4 = double.parse(nA);
+      n4 = double.parse(nD);
     } else if (i == 4) {
       print("Informe o ${i + 1}º numero ");
-      nA = stdin.readLineSync();
-      n1 = double.parse(nA);
+      nE = stdin.readLineSync();
+      n5 = double.parse(nE);
     }
   }
+  print("O resultado = ${(n1 + n2) + (n3) + (n4 + n5)}");
 }
