030b1b1f3773289c0a001dbf72107980b155ef24
RicMerces <ricardoliveira0703@gmail.com>
2021-11-09 20:02:38 -0300

    Desafio12

---
+++ dart/Challenges/Parte03/Desafio12.dart
@@ -0,0 +1,17 @@
+import 'dart:io';
+
+void main() {
+  for (int i = 1; i <= 10; i++) {
+    print("\nTabuada de $i");
+    print("$i X 1 = ${i * 1}");
+    print("$i X 2 = ${i * 2}");
+    print("$i X 3 = ${i * 3}");
+    print("$i X 4 = ${i * 4}");
+    print("$i X 5 = ${i * 5}");
+    print("$i X 6 = ${i * 6}");
+    print("$i X 7 = ${i * 7}");
+    print("$i X 8 = ${i * 8}");
+    print("$i X 9 = ${i * 9}");
+    print("$i X 10 = ${i * 10}");
+  }
+}
