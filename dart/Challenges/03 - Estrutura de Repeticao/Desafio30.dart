ea73f377485958d70c3df02b28f90799858f4b96
RicMerces <ricardoliveira0703@gmail.com>
2021-11-13 09:22:39 -0300

    Desasfio30

--- dart/Challenges/03 - Estrutura de Repeticao/Desafio30.dart
+++ dart/Challenges/03 - Estrutura de Repeticao/Desafio30.dart
@@ -1,3 +1,20 @@
 import 'dart:io';
 
-void main() {}
+void main() {
+  print("Informe o numero de pães que você deseja comprar ");
+  var pao = stdin.readLineSync()!;
+  int paes = int.parse(pao);
+  while (paes > 50 || paes < 1) {
+    print("Digite novamente \no valor deve ser entre 1 a 50");
+    var pao = stdin.readLineSync()!;
+    int paes = int.parse(pao);
+  }
+  double preco = 0;
+  for (int i = 1; i <= paes; i++) {
+    preco += 0.18;
+
+    if (i == paes) {
+      print("$i - $preco Reais");
+    }
+  }
+}
