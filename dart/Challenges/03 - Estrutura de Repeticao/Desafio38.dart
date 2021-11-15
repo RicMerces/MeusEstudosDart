e745afe7665e5e38a8d144aab519a0f9bb79c23a
RicMerces <ricardoliveira0703@gmail.com>
2021-11-15 12:20:26 -0300

    Desafio38

--- dart/Challenges/03 - Estrutura de Repeticao/Desafio38.dart
+++ dart/Challenges/03 - Estrutura de Repeticao/Desafio38.dart
@@ -1,11 +1,39 @@
 import 'dart:io';
 
 void main() {
-  double sInicial = 1000;
-  int anoInicial = 1995;
+  print("Informe seu nome");
+  String nome = stdin.readLineSync()!;
   double sFinal = 0;
-  for (int i = 1; i <= 2021; i++) {
-    sFinal = sInicial * (0.015 * i);
+  int x = 1;
+  //Caso de Afonso (Parte 01)
+  if (nome.toUpperCase() == 'Afonso'.toUpperCase()) {
+    double sInicial = 1000;
+    int anoInicial = 1995;
+    for (int i = anoInicial; i <= 2021; i++) {
+      sFinal = sInicial + (sInicial * (0.015 * (2 * x)));
+      x++;
+    }
   }
-  print("O salario atual dele em 2021 = $sFinal");
+  //Parte 02
+  else {
+    print("Informe seu salario inicial");
+    var salarioInicial = stdin.readLineSync()!;
+    double sInicial = double.parse(salarioInicial);
+    print("Informe o ano que você começou a trabalhar");
+    var primeiroAno = stdin.readLineSync()!;
+    int anoInicial = int.parse(primeiroAno);
+
+    while (anoInicial > 2021) {
+      print("Não aceitamos numeros posterior a 2021");
+      var primeiroAno = stdin.readLineSync()!;
+      anoInicial = int.parse(primeiroAno);
+    }
+
+    for (int i = anoInicial; i <= 2021; i++) {
+      sFinal = sInicial + (sInicial * (0.015 * (2 * x)));
+      x++;
+    }
+  }
+
+  print("O salario atual dele em 2021 = $sFinal Reais");
 }
