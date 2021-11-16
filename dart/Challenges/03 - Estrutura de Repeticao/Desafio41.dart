c0080c77269e6190d91e9a464674bb094f307896
RicMerces <ricardoliveira0703@gmail.com>
2021-11-16 18:39:46 -0300

    Desafio41

--- dart/Challenges/03 - Estrutura de Repeticao/Desafio41.dart
+++ dart/Challenges/03 - Estrutura de Repeticao/Desafio41.dart
@@ -2,7 +2,7 @@ import 'dart:io';
 
 void main() {
   int nDeParcelas = 1;
-  int juros = 0;
+  double juros = 0;
   while (nDeParcelas != 0) {
     print("Informe o valor do produto que você deseja comprar");
     var valorProduto = stdin.readLineSync()!;
@@ -10,29 +10,29 @@ void main() {
 
     print("Informe o valor de parcelas que você deseja dividir");
     var numeroDeParcelas = stdin.readLineSync()!;
-    int nDeParcelas = int.parse(numeroDeParcelas);
+    nDeParcelas = int.parse(numeroDeParcelas);
 
-    while (nDeParcelas > 0) {
+    while (nDeParcelas < 0) {
       print("Informe o valor de parcelas que você deseja dividir");
       var numeroDeParcelas = stdin.readLineSync()!;
       nDeParcelas = int.parse(numeroDeParcelas);
     }
     if (nDeParcelas > 1 && nDeParcelas <= 3) {
-      juros = 10;
+      juros = productValue * (0.1);
     }
     if (nDeParcelas > 3 && nDeParcelas <= 6) {
-      juros = 15;
+      juros = productValue * (0.15);
     }
     if (nDeParcelas > 6 && nDeParcelas <= 9) {
-      juros = 20;
+      juros = productValue * (0.2);
     }
     if (nDeParcelas > 9 && nDeParcelas <= 12) {
-      juros = 25;
+      juros = productValue * (0.25);
     }
     if (nDeParcelas > 12) {
-      juros = 30;
+      juros = productValue * (0.3);
     }
     print(
-        "Valor da divida = $productValue Reais\nValor dos Juros = $juros\nQuantidade de Parcelas = $nDeParcelas ");
+        "Valor da divida = $productValue Reais\nValor dos Juros = $juros\nQuantidade de Parcelas = $nDeParcelas\nValor de parcela = ${(productValue / nDeParcelas)} Reais");
   }
 }
