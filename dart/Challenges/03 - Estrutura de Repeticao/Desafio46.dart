56a5a17d0a81e2c0375100422d3d8070496ce35a
RicMerces <ricardoliveira0703@gmail.com>
2021-11-18 22:11:50 -0300

    Desafio46

---
+++ dart/Challenges/03 - Estrutura de Repeticao/Desafio46.dart
@@ -0,0 +1,35 @@
+import 'dart:io';
+
+void main() {
+  print("Informe seu nome");
+  String nome = stdin.readLineSync()!;
+  double dist = 0;
+
+  double max = 0;
+  double min = 0;
+
+  double x = 0;
+  double media = 0;
+
+  for (int i = 1; i <= 5; i++) {
+    print("Informe qual foi a distancia no $iº Salto");
+    var distancia = stdin.readLineSync()!;
+    dist = double.parse(distancia);
+
+    if (dist > max) {
+      max = dist;
+    }
+
+    if (i == 1 || dist < min) {
+      min = dist;
+    }
+
+    media = dist + x;
+    x = media;
+  }
+
+  print("\n$nome");
+  print("Nota maxima = $max");
+  print("Nota minima = $min");
+  print("Media = ${media / 5}");
+}
