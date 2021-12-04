a8e513b2f8ef60d7f38e512d2fb5f1c0fd9afaa3
RicMerces <ricardoliveira0703@gmail.com>
2021-12-04 12:31:17 -0300

    Desafio18

--- dart/Challenges/04 - Listas/Desafio18.dart
+++ dart/Challenges/04 - Listas/Desafio18.dart
@@ -1,10 +1,88 @@
 import 'dart:io';
 
 void main() {
-// Uma grande emissora de televisão quer fazer uma enquete entre os seus telespectadores para saber qual o melhor jogador após cada jogo. Para isto, faz-se necessário o desenvolvimento de um programa, que será utilizado pelas telefonistas, para a computação dos votos. Sua equipe foi contratada para desenvolver este programa, utilizando a linguagem de programação C++. Para computar cada voto, a telefonista digitará um número, entre 1 e 23, correspondente ao número da camisa do jogador. Um número de jogador igual zero, indica que a votação foi encerrada. Se um número inválido for digitado, o programa deve ignorá-lo, mostrando uma breve mensagem de aviso, e voltando a pedir outro número. Após o final da votação, o programa deverá exibir:
-// O total de votos computados;
-// Os númeos e respectivos votos de todos os jogadores que receberam votos;
-// O percentual de votos de cada um destes jogadores;
-// O número do jogador escolhido como o melhor jogador da partida, juntamente com o número de votos e o percentual de votos dados a ele.
-// Observe que os votos inválidos e o zero final não devem ser computados como votos. O resultado aparece ordenado pelo número do jogador. O programa deve fazer uso de arrays. O programa deverá executar o cálculo do percentual de cada jogador através de uma função. Esta função receberá dois parâmetros: o número de votos de um jogador e o total de votos. A função calculará o percentual e retornará o valor calculado. Abaixo segue uma tela de exemplo. O disposição das informações deve ser o mais próxima possível ao exemplo. Os dados são fictícios e podem mudar a cada execução do programa. Ao final, o programa deve ainda gravar os dados referentes ao resultado da votação em um arquivo texto no disco, obedecendo a mesma disposição apresentada na tela.
+  int valor = 1;
+  List j1 = [];
+  List j2 = [];
+  List j3 = [];
+  List j4 = [];
+  List j5 = [];
+  List j6 = [];
+  List j7 = [];
+  List j8 = [];
+  List j9 = [];
+  List j10 = [];
+  List j11 = [];
+  List j12 = [];
+  List j13 = [];
+  List j14 = [];
+  List j15 = [];
+  List j16 = [];
+  List j17 = [];
+  List j18 = [];
+  List j19 = [];
+  List j20 = [];
+  List j21 = [];
+  List j22 = [];
+
+  while (valor != 0) {
+    print("Informe o seu nome");
+    String nome = stdin.readLineSync()!;
+
+    print("Informe o numero do jogador");
+    var numero = stdin.readLineSync()!;
+    valor = int.parse(numero);
+
+    while (valor > 22 || valor < 0) {
+      print("Esse jogador não existe ou não jogou esta partida");
+      var numero = stdin.readLineSync()!;
+      valor = int.parse(numero);
+    }
+
+    if (valor == 1) {
+      j1.add(nome);
+    } else if (valor == 2) {
+      j2.add(nome);
+    } else if (valor == 3) {
+      j3.add(nome);
+    } else if (valor == 4) {
+      j4.add(nome);
+    } else if (valor == 5) {
+      j5.add(nome);
+    } else if (valor == 6) {
+      j6.add(nome);
+    } else if (valor == 7) {
+      j7.add(nome);
+    } else if (valor == 8) {
+      j8.add(nome);
+    } else if (valor == 9) {
+      j9.add(nome);
+    } else if (valor == 10) {
+      j10.add(nome);
+    } else if (valor == 11) {
+      j11.add(nome);
+    } else if (valor == 12) {
+      j12.add(nome);
+    } else if (valor == 13) {
+      j13.add(nome);
+    } else if (valor == 14) {
+      j14.add(nome);
+    } else if (valor == 15) {
+      j15.add(nome);
+    } else if (valor == 16) {
+      j16.add(nome);
+    } else if (valor == 17) {
+      j17.add(nome);
+    } else if (valor == 18) {
+      j18.add(nome);
+    } else if (valor == 19) {
+      j19.add(nome);
+    } else if (valor == 20) {
+      j20.add(nome);
+    } else if (valor == 21) {
+      j21.add(nome);
+    } else if (valor == 22) {
+      j22.add(nome);
+    }
+  }
 }
