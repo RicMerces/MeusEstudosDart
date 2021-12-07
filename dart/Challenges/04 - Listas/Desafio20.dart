db1ede06ae63a4e49eb927559d7cfbe89d4ab4ee
RicMerces <ricardoliveira0703@gmail.com>
2021-12-07 14:54:36 -0300

    Desafio20

--- dart/Challenges/04 - Listas/Desafio20.dart
+++ dart/Challenges/04 - Listas/Desafio20.dart
@@ -2,13 +2,23 @@ import 'dart:io';
 
 void main() {
   List arbono = [];
+  double z = 0;
+  double w = 0;
   for (int i = 0; i < 4; i++) {
     print("Informe seu salario");
     var salario = stdin.readLineSync()!;
     double money = double.parse(salario);
 
-    if(int x = 0)
+    arbono.add(money * 0.05);
 
-
+    if (i == 3) {
+      for (int x = 0; x < arbono.length; x++) {
+        double y = arbono[x];
+        z = y + w;
+        w = z;
+      }
+    }
   }
+
+  print("Arbono final pago = $w");
 }
