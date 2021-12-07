67004bb27c45b462768e7dfe086fab26e95c5a7e
RicMerces <ricardoliveira0703@gmail.com>
2021-12-07 18:21:21 -0300

    Desafio21

--- dart/Challenges/04 - Listas/Desafio21.dart
+++ dart/Challenges/04 - Listas/Desafio21.dart
@@ -1,32 +1,30 @@
 import 'dart:io';
 
 void main() {
-// Faça um programa que carregue uma lista com os modelos de cinco carros (exemplo de modelos: FUSCA, GOL, VECTRA etc). Carregue uma outra lista com o consumo desses carros, isto é, quantos quilômetros cada um desses carros faz com um litro de combustível. Calcule e mostre:
-// O modelo do carro mais econômico;
-// Quantos litros de combustível cada um dos carros cadastrados consome para percorrer uma distância de 1000 quilômetros e quanto isto custará, considerando um que a gasolina custe R$ 2,25 o litro. Abaixo segue uma tela de exemplo. O disposição das informações deve ser o mais próxima possível ao exemplo. Os dados são fictícios e podem mudar a cada execução do programa.
-// Comparativo de Consumo de Combustível
+  List veiculos = [];
+  List kmporlitro = [];
+  List litragem = [];
+  List precos = [];
 
-// Veículo 1
-// Nome: fusca
-// Km por litro: 7
-// Veículo 2
-// Nome: gol
-// Km por litro: 10
-// Veículo 3
-// Nome: uno
-// Km por litro: 12.5
-// Veículo 4
-// Nome: Vectra
-// Km por litro: 9
-// Veículo 5
-// Nome: Peugeout
-// Km por litro: 14.5
+  print("Informe a quantidade de veiculos");
+  var nDeVeiculos = stdin.readLineSync()!;
 
-// Relatório Final
-//  1 - fusca           -    7.0 -  142.9 litros - R$ 321.43
-//  2 - gol             -   10.0 -  100.0 litros - R$ 225.00
-//  3 - uno             -   12.5 -   80.0 litros - R$ 180.00
-//  4 - vectra          -    9.0 -  111.1 litros - R$ 250.00
-//  5 - peugeout        -   14.5 -   69.0 litros - R$ 155.17
-// O menor consumo é do peugeout.
+  for (int i = 0; i < int.parse(nDeVeiculos); i++) {
+    print("Veiculo = ${i + 1}");
+
+    print("Informe o nome do veiculo");
+    String nome = stdin.readLineSync()!;
+    veiculos.add(nome);
+
+    print("Informe a medida de km por litro");
+    var medida = stdin.readLineSync()!;
+    double km = double.parse(medida);
+    kmporlitro.add(km);
+
+    double litros = (1000 / km);
+    litragem.add(litros);
+
+    double preco = (litros * 2.25);
+    precos.add(preco);
+  }
 }
