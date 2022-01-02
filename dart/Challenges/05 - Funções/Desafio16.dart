13188180dd1c30cd37916f5dbe315363f789da9d
RicMerces <ricardoliveira0703@gmail.com>
2022-01-02 17:56:52 -0300

    Desafio16

--- dart/Challenges/05 - Funções/Desafio16.dart
+++ dart/Challenges/05 - Funções/Desafio16.dart
@@ -1,1 +1,24 @@
+import 'dart:io';
 
+void main() {
+  print("Informe o valor do limite");
+  var limites = stdin.readLineSync()!;
+  int limite = int.parse(limites);
+
+  imprimir(limite);
+}
+
+String imprimir(int l) {
+  String texto = '';
+  for (int i = 1; i <= l; i++) {
+    String aux = '';
+    for (int x = 1; x <= i; x++) {
+      texto = '$aux' + '$i';
+      aux = texto;
+    }
+
+    print(texto);
+  }
+
+  return texto;
+}
