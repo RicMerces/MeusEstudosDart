416834d092bf5eabe077c8f82300ba24d8b84ac2
RicMerces <ricardoliveira0703@gmail.com>
2022-01-02 18:09:57 -0300

    Desafio3

--- dart/Challenges/05 - Funções/Desafio03.dart
+++ dart/Challenges/05 - Funções/Desafio03.dart
@@ -1,3 +1,21 @@
 import 'dart:io';
 
-void main() {}
+void main() {
+  print("Informe três valores");
+
+  var va1 = stdin.readLineSync()!;
+  var va2 = stdin.readLineSync()!;
+  var va3 = stdin.readLineSync()!;
+
+  double v1 = double.parse(va1);
+  double v2 = double.parse(va2);
+  double v3 = double.parse(va3);
+
+  operacao(v1, v2, v3);
+}
+
+double operacao(double x, double y, double z) {
+  double r = ((x + y) + z);
+  print(r);
+  return r;
+}
