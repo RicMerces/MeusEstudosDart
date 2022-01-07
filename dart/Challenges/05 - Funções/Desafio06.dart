ec91e9dce230c8e1edd9db6a1697ebf324388c14
RicMerces <ricardoliveira0703@gmail.com>
2022-01-07 11:22:09 -0300

    Desafio06

--- dart/Challenges/05 - Funções/Desafio06.dart
+++ dart/Challenges/05 - Funções/Desafio06.dart
@@ -4,7 +4,7 @@ void main() {
   receberDados();
 }
 
-String receberDados(){
+String receberDados() {
   print("Informe o horario de agora");
   var horario = stdin.readLineSync()!;
   validacao(horario);
@@ -12,26 +12,44 @@ String receberDados(){
   return horario;
 }
 
-
-String horas(String h) {
+String alterarHoras(String h) {
   h.split('');
   int hora = int.parse(h[0] + h[1]);
+  String periodo = '';
+  if (hora > 12) {
+    hora = hora - 12;
+    periodo = 'da Tarde';
+  } else {
+    periodo = 'da Manhã';
+  }
 
-  hora = hora - 12;
-
-  h = "$hora:${h[3]}${h[4]}";
-  print(h);
+  h = "$hora:${h[3]}${h[4]} ";
+  print(h + periodo);
   return h;
 }
 
-
-String validacao (String h){
-  while(h.length > 5){
+String validacao(String h) {
+  while (h.length != 5) {
     receberDados();
   }
+
   var hora = h.split('');
-  String n = hora[0] + hora[1];
-  while()
+
+  String n1 = hora[0] + hora[1];
+  String n2 = hora[3] + hora[4];
+
+  while (int.parse(n1) > 24) {
+    receberDados();
+  }
+
+  while (int.parse(n2) > 59) {
+    receberDados();
+  }
+
+  while (hora[2] != ':') {
+    receberDados();
+  }
+
+  alterarHoras(h);
+  return h;
 }
-
-
