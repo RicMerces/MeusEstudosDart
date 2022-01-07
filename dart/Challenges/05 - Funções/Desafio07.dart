cbd03951082982ef916c96d504aedb15fb26591a
RicMerces <ricardoliveira0703@gmail.com>
2022-01-07 15:39:09 -0300

    Desafio07

--- dart/Challenges/05 - Funções/Desafio07.dart
+++ dart/Challenges/05 - Funções/Desafio07.dart
@@ -1,3 +1,24 @@
 import 'dart:io';
 
-void main() {}
+void main() {
+  valorPagamento();
+}
+
+String valorPagamento() {
+  print("Determine o valor a ser pago");
+  var valor = stdin.readLineSync()!;
+
+  print("Houve atraso ? \nDIGITE:\ns\nn");
+  var j = stdin.readLineSync()!;
+
+  while (j != 's' || j != 'n') {
+    print("RESPOSTA INVALIDA");
+    print("Houve atraso ? \nDIGITE:\ns\nn");
+    var j = stdin.readLineSync()!;
+  }
+  if (j == 's') {
+    atrasar();
+  }
+
+  return valor;
+}
