dd53fd2bd9e5020a5bec17b3f1e5498b4cc4b594
RicMerces <ricardoliveira0703@gmail.com>
2022-01-10 21:14:50 -0300

    Desafio10

--- dart/Challenges/05 - Funções/Desafio10.dart
+++ dart/Challenges/05 - Funções/Desafio10.dart
@@ -2,29 +2,44 @@ import 'dart:io';
 import 'dart:math';
 
 void main() {
-  print("BEM VINDO AO CRAPS\nUM JOGO DE SORTE");
-  var resposta = '';
-
-  while (resposta.toLowerCase() != 'n') {
-    print(
-        "Deseja iniciar o jogo ?\nDigite (s) para jogar Digite (n) para não jogar");
-    resposta = stdin.readLineSync()!;
-    if (resposta == 's') {
-      jogar();
-    }
+  var iniciar = 's';
+  while (iniciar == 's') {
+    print("Iniciar Jogo de Craps");
+    iniciar = stdin.readLineSync()!;
+    var resultado = iniciar == 's' ? iniciarJogo() : "Fim";
+    print(resultado);
   }
 }
 
-String jogar() {
-  print("Informe um valor entre 2 e 12");
-  var valor = stdin.readLineSync()!;
+String iniciarJogo() {
+  String resultado = '';
+  print("Informe dois valores (Entre 2 e 12)");
+  var v01 = stdin.readLineSync()!;
+  var v02 = stdin.readLineSync()!;
+  int v1 = int.parse(v01);
+  int v2 = int.parse(v02);
 
-  while (int.parse(valor) > 12 || int.parse(valor) < 2) {
-    print("\nVALOR INVALIDO\nInforme um valor entre 2 e 12");
-    valor = stdin.readLineSync()!;
+  while (v1 > 12 || v1 < 2 || v2 > 12 || v2 < 2) {
+    print("Os valores devem ser Entre 2 e 12");
+    v01 = stdin.readLineSync()!;
+    v02 = stdin.readLineSync()!;
+    v1 = int.parse(v01);
+    v2 = int.parse(v02);
   }
 
-  sorteio();
+  int z = realizarSorteio();
+
+  resultado = z == v1 || z == v2 ? 'Você ganhou o jogo !!' : 'Você perdeu';
+
+  return resultado;
 }
 
-int sorteio() {}
+int realizarSorteio() {
+  var rng = new Random();
+  int x = rng.nextInt(12);
+  while (x < 2) {
+    rng.nextInt(12);
+  }
+  print("O valor sorteado foi $x");
+  return x;
+}
