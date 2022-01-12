19a89765d5d1663c8f30354217d65f9e50d0a02b
RicMerces <ricardoliveira0703@gmail.com>
2022-01-12 20:14:15 -0300

    Desafio12

--- dart/Challenges/05 - Funções/Desafio12.dart
+++ dart/Challenges/05 - Funções/Desafio12.dart
@@ -1,3 +1,34 @@
 import 'dart:io';
+import 'dart:math';
 
-void main() {}
+void main() {
+  imprimir();
+}
+
+String imprimir() {
+  print("Informe uma palavra");
+  var p = stdin.readLineSync()!;
+
+  var palavra = embaralhar(p);
+
+  print(palavra);
+  return palavra;
+}
+
+String embaralhar(var p) {
+  var palavra = p.split('');
+  var rng = new Random();
+
+  int t = palavra.length;
+
+  String p1 = '';
+  String plus = '';
+
+  for (int i = 0; i < t; i++) {
+    int x = rng.nextInt(t);
+    p1 = '${palavra[x]}$plus';
+    plus = p1;
+  }
+
+  return p1;
+}
