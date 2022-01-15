de458301eaffab87adf9a5a30dba01c18e9cb1f4
RicMerces <ricardoliveira0703@gmail.com>
2022-01-15 19:45:39 -0300

    Desafio01

--- dart/Challenges/06 - Map & Reduce + Filter/Desafio01.dart
+++ dart/Challenges/06 - Map & Reduce + Filter/Desafio01.dart
@@ -1,19 +1,21 @@
 import 'dart:io';
 
 void main() {
+  List lista = [];
+  print("INICIAR");
   for (int i = 0; i < 5; i++) {
     print("Informe o ${i + 1}º valor da lista");
     var valor = stdin.readLineSync()!;
     int v = int.parse(valor);
 
-    adicionarNaLista(v);
+    adicionarNaLista(v, lista);
   }
 }
 
-String adicionarNaLista(int v) {
-  List lista = [];
+String adicionarNaLista(int v, List lista) {
   lista.add(v);
 
   String resultado = 'A Lista final = $lista';
+  print(resultado);
   return resultado;
 }
