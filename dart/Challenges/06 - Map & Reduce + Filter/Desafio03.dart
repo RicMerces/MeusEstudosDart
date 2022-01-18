36ae9241277fc7af05b8d6edbd3c4d720add9703
RicMerces <ricardoliveira0703@gmail.com>
2022-01-18 20:59:09 -0300

    Desafio03

--- dart/Challenges/06 - Map & Reduce + Filter/Desafio03.dart
+++ dart/Challenges/06 - Map & Reduce + Filter/Desafio03.dart
@@ -1,3 +1,25 @@
 import 'dart:io';
 
-void main() {}
+void main() {
+  imprimir();
+}
+
+String imprimir() {
+  String nome = informeNome();
+  print("Bem Vindo ao calculadora de notas $nome");
+
+  String resultados = informeNotas();
+}
+
+String informeNome() {
+  print("Informe seu nome ");
+  String nome = stdin.readLineSync()!;
+  return nome;
+}
+
+String informeNotas() {
+  List notas = [];
+  for (int i = 0; i < 4; i++) {
+    print("Informe a nota do ${i + 1} Bimestre");
+  }
+}
