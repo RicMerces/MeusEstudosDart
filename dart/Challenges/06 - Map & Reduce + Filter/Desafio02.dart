0e05a2ab9105a2b501f10c8bcfcf534a239d979e
RicMerces <ricardoliveira0703@gmail.com>
2022-01-18 17:43:01 -0300

    Desafio02

--- dart/Challenges/06 - Map & Reduce + Filter/Desafio02.dart
+++ dart/Challenges/06 - Map & Reduce + Filter/Desafio02.dart
@@ -1,13 +1,41 @@
 import 'dart:io';
 
+import '../05 - Funções/Desafio09.dart';
+
 void main() {
   informeOsValores();
 }
 
 String informeOsValores() {
-    for(int i = 0; i < 10; i++){
-      print("Informe o primeiro valor $i")
-    }
+  String resultado = '';
+  for (int i = 0; i < 10; i++) {
+    print("Informe o ${i + 1} valor");
+    var v = stdin.readLineSync()!;
 
+    resultado = adicionarALista(int.parse(v), i);
+  }
 
+  return resultado;
+}
+
+String adicionarALista(int valor, int cont) {
+  String resultado = '';
+  List lista = [];
+  lista.add(valor);
+
+  if (cont == 9) {
+    resultado = imprimir(lista);
+  }
+  return resultado;
+}
+
+String imprimir(List l) {
+  String texto = '';
+  String aux = '';
+  for (int i = 0; i < l.length; i++) {
+    texto = aux + l[i];
+    aux = texto;
+  }
+  print("$texto");
+  return texto;
 }
