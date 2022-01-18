8a2de761216375a4c688484ff54013c8c96d4d7c
RicMerces <ricardoliveira0703@gmail.com>
2022-01-18 00:35:04 -0300

    Desafio02

--- dart/Challenges/06 - Map & Reduce + Filter/Desafio02.dart
+++ dart/Challenges/06 - Map & Reduce + Filter/Desafio02.dart
@@ -4,4 +4,10 @@ void main() {
   informeOsValores();
 }
 
-String informeOsValores() {}
+String informeOsValores() {
+    for(int i = 0; i < 10; i++){
+      print("Informe o primeiro valor $i")
+    }
+
+
+}
