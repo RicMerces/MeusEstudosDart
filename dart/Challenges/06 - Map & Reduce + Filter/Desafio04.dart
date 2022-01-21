a5c4228d79a232cd5dc4c4bae98664f1ecb440a3
RicMerces <ricardoliveira0703@gmail.com>
2022-01-21 15:48:32 -0300

    Desafio04

--- dart/Challenges/06 - Map & Reduce + Filter/Desafio04.dart
+++ dart/Challenges/06 - Map & Reduce + Filter/Desafio04.dart
@@ -7,20 +7,52 @@ void main() {
 String informar() {
   print("Informe uma palavra");
   String frase = stdin.readLineSync()!;
+  String fraseFinal = contadorDeConsoantes(frase);
 
-  contadorDeConsoantes(frase);
+  print("$fraseFinal");
+  return fraseFinal;
 }
 
 String contadorDeConsoantes(String f) {
-  List consoantes = [B,C,D,F,G,H,J,K,L,M,N,P,Q,R,S,T,V,W,X,Y,Z];
-
+  List consoantes = [
+    'B',
+    'C',
+    'D',
+    'F',
+    'G',
+    'H',
+    'J',
+    'K',
+    'L',
+    'M',
+    'N',
+    'P',
+    'Q',
+    'R',
+    'S',
+    'T',
+    'V',
+    'W',
+    'X',
+    'Y',
+    'Z'
+  ];
+  List ListaFinal = [];
   var frase = f.split('');
-  for (int i =0; i < f.length; i++){
-    while(frase[i] in consoantes ){
-      
+  for (int i = 0; i < f.length; i++) {
+    ListaFinal = verificar(consoantes, frase[i]);
+  }
+  String resultado = 'Os valores finais = ${ListaFinal}';
+  return resultado;
+}
+
+List verificar(List c, String f) {
+  List listaFinal = [];
+  for (int i = 0; i < c.length; i++) {
+    if (f == c[i]) {
+      listaFinal.add(f);
     }
   }
 
-
-
+  return listaFinal;
 }
