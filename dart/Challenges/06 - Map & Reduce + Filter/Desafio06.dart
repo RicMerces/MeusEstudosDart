90bd37987100a3f12dcf4fd0c8fa5175f05b4404
RicMerces <ricardoliveira0703@gmail.com>
2022-01-26 12:09:37 -0300

    Desafio06

--- dart/Challenges/06 - Map & Reduce + Filter/Desafio06.dart
+++ dart/Challenges/06 - Map & Reduce + Filter/Desafio06.dart
@@ -1,20 +1,41 @@
 import 'dart:io';
 
 void main() {
-  quantidadeDeAlunos();
+  imprimir();
 }
 
-String informeDados(List alunos) {
+Map informeDados(Map alunos) {
   print("Informe seu nome ");
   var nome = stdin.readLineSync()!;
-  alunos.add(nome);
-  calcularMedia();
+  double media = calcularMedia();
+  alunos['$nome'] = media;
+
+  String 
   return nome;
 }
 
-int quantidadeDeAlunos() {
-  List alunos = [];
+String imprimir() {
+  Map alunos = {};
   for (int i = 0; i < 10; i++) {
     informeDados(alunos);
   }
+}
+
+double calcularMedia() {
+  double j = 0;
+  double total = 0;
+  for (int i = 0; i < 4; i++) {
+    double nota = informarMedia(i);
+    total = nota + j;
+    j = total;
+  }
+
+  return total;
+}
+
+double informarMedia(int i) {
+  print("Informe sua nota do ${i + 1}º Bimestre");
+  var nota = stdin.readLineSync()!;
+
+  return double.parse(nota);
 }
