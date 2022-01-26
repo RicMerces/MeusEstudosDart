1997081a7a2fdcacda7cdb3626a40f5719ae55a4
RicMerces <ricardoliveira0703@gmail.com>
2022-01-26 10:25:56 -0300

    Desafio06

--- dart/Challenges/06 - Map & Reduce + Filter/Desafio06.dart
+++ dart/Challenges/06 - Map & Reduce + Filter/Desafio06.dart
@@ -1,1 +1,20 @@
-void main() {}
+import 'dart:io';
+
+void main() {
+  quantidadeDeAlunos();
+}
+
+String informeDados(List alunos) {
+  print("Informe seu nome ");
+  var nome = stdin.readLineSync()!;
+  alunos.add(nome);
+  calcularMedia();
+  return nome;
+}
+
+int quantidadeDeAlunos() {
+  List alunos = [];
+  for (int i = 0; i < 10; i++) {
+    informeDados(alunos);
+  }
+}
