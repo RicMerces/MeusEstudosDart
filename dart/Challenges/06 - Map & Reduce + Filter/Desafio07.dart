18dd6fb071b73ade46cb48f27f0167489c74d5b2
RicMerces <ricardoliveira0703@gmail.com>
2022-01-27 21:12:05 -0300

    Desafio07

--- dart/Challenges/06 - Map & Reduce + Filter/Desafio07.dart
+++ dart/Challenges/06 - Map & Reduce + Filter/Desafio07.dart
@@ -1,1 +1,12 @@
-void main() {}
+import 'dart:io';
+
+void main() {
+  receberNumeros();
+}
+
+double receberNumeros() {
+  for (int i = 0; i < 5; i++) {
+    print("Informe o numero ${i + 1}");
+    var num = stdin.readLineSync()!;
+  }
+}
