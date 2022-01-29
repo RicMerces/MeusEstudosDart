a7d025521fa669cb3b99c5dd30643075afc51391
RicMerces <ricardoliveira0703@gmail.com>
2022-01-29 15:39:13 -0300

    Desafio08

--- dart/Challenges/06 - Map & Reduce + Filter/Desafio08.dart
+++ dart/Challenges/06 - Map & Reduce + Filter/Desafio08.dart
@@ -1,1 +1,27 @@
-void main() {}
+import 'dart:io';
+
+void main() {
+  informarValores();
+}
+
+String informarValores() {
+  Map identidade = {};
+
+  for (int i = 0; i < 1; i++) {
+    print("Informe seu nome");
+    String nome = stdin.readLineSync()!;
+
+    print("Informe sua idade");
+    var idade = stdin.readLineSync()!;
+    int id = int.parse(idade);
+
+    print("Informe sua altura");
+    var altura = stdin.readLineSync()!;
+    double h = double.parse(altura);
+
+    identidade['$nome'] = 'Idade = $id  Altura = $h';
+  }
+
+  print(identidade);
+  return '$identidade';
+}
