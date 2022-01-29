8cbe05e069e64207671b33fe6f67437b1afb353f
RicMerces <ricardoliveira0703@gmail.com>
2022-01-29 16:31:23 -0300

    Desafio09

--- dart/Challenges/06 - Map & Reduce + Filter/Desafio09.dart
+++ dart/Challenges/06 - Map & Reduce + Filter/Desafio09.dart
@@ -1,3 +1,33 @@
 import 'dart:io';
 
-void main() {}
+void main() {
+  informarValores();
+}
+
+String informarValores() {
+  List listaDeNumeros = [];
+  for (int i = 0; i < 5; i++) {
+    print("Informe um numero");
+    var numero = stdin.readLineSync()!;
+    int n = int.parse(numero);
+
+    listaDeNumeros.add(n);
+  }
+
+  double soma = somarValores(listaDeNumeros);
+
+  print(soma);
+  return '$soma';
+}
+
+double somarValores(List l) {
+  double x = 0;
+  double aux = 0;
+
+  for (int i = 0; i < l.length; i++) {
+    x = (l[i] * l[i]) + aux;
+    aux = x;
+  }
+
+  return x;
+}
