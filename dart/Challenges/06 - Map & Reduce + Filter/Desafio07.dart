9cbc87bf0eb32323f89904edbf43ea4dd91d3f92
RicMerces <ricardoliveira0703@gmail.com>
2022-01-29 14:27:12 -0300

    Desafio07

--- dart/Challenges/06 - Map & Reduce + Filter/Desafio07.dart
+++ dart/Challenges/06 - Map & Reduce + Filter/Desafio07.dart
@@ -1,12 +1,44 @@
 import 'dart:io';
 
 void main() {
-  receberNumeros();
+  informeValores();
 }
 
-double receberNumeros() {
+String informeValores() {
+  List valores = [];
   for (int i = 0; i < 5; i++) {
-    print("Informe o numero ${i + 1}");
-    var num = stdin.readLineSync()!;
+    print("Informe o ${i + 1}º valor");
+    var valor = stdin.readLineSync()!;
+    int v = int.parse(valor);
+
+    valores.add(v);
   }
+
+  int soma = realizarSoma(valores);
+  int multiplicacao = realizarMultiplicacao(valores);
+
+  String finalizado = 'Soma (+) = $soma\nMultiplicação (*) = $multiplicacao';
+  print(finalizado);
+  return finalizado;
+}
+
+int realizarSoma(List v) {
+  int x = 0;
+  int aux = 0;
+  for (int i = 0; i < v.length; i++) {
+    x = v[i] + aux;
+    aux = x;
+  }
+  return x;
+}
+
+int realizarMultiplicacao(List v) {
+  int x = 1;
+  int aux = 1;
+  for (int i = 0; i < v.length; i++) {
+    x = v[i] * aux;
+    aux = x;
+  }
+
+  return x;
 }
