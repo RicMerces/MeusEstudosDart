bb92966f65b3cc065d9a4f88c9d344c9deb01934
RicMerces <ricardoliveira0703@gmail.com>
2022-01-30 13:46:46 -0300

    Desafio12

--- dart/Challenges/06 - Map & Reduce + Filter/Desafio12.dart
+++ dart/Challenges/06 - Map & Reduce + Filter/Desafio12.dart
@@ -1,3 +1,39 @@
 import 'dart:io';
 
-void main() {}
+void main() {
+  informeDadosAlunos();
+}
+
+String informeDadosAlunos() {
+  Map dadosAlunos = {};
+  List idades = [];
+  List nomes = [];
+  List alturas = [];
+
+  List alertAlunos = [];
+
+  for (int i = 0; i < 4; i++) {
+    print("Informe nome do aluno ");
+    var nome = stdin.readLineSync()!;
+
+    print("Informe idade de $nome ");
+    var idade = stdin.readLineSync()!;
+    int id = int.parse(idade);
+
+    print("Informe a altura de $nome em m");
+    var altura = stdin.readLineSync()!;
+    double h = double.parse(altura);
+
+    nomes.add(nome);
+    idades.add(id);
+    alturas.add(h);
+
+    dadosAlunos[nome] = ' | Idade = $idade | Altura = $h | ';
+
+    verificar(alertAlunos, nome, id, h);
+  }
+}
+
+List verificar(List alunos, String nome, int id, double h) {
+  return alunos;
+}
