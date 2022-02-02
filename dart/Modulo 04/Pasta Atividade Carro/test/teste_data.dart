c0bcec3da3fde942c01c60ea3dc173213534cda5
RicMerces <ricardoliveira0703@gmail.com>
2022-02-02 14:09:41 -0300

    Desafio carro  pt2

--- dart/Modulo 04/Pasta Atividade Carro/test/teste_data.dart
+++ dart/Modulo 04/Pasta Atividade Carro/test/teste_data.dart
@@ -5,5 +5,25 @@ main() {
   print("Informe qual a velocidade maxima do modelo de veiculo");
   var vMax = stdin.readLineSync()!;
   int vM = int.parse(vMax);
-  var seuCarro = new Data(vM, 1000);
+
+  print("Informe a velocidade atual");
+  var vAtual = stdin.readLineSync()!;
+  int vH = int.parse(vAtual);
+
+  var seuCarro = new Data(vM, vH);
+
+  while (seuCarro.velocidadeAtual > 0) {
+    print("Deseja acelerar seu carro Digite (1)");
+    print("Deseja desacelerar seu carro Digite (2)");
+    var escolha = stdin.readLineSync()!;
+
+    if (int.parse(escolha) == 1) {
+      print("Acelerar !");
+      seuCarro.acelerar();
+    } else if (int.parse(escolha) == 2) {
+      seuCarro.freio();
+    }
+
+    print("$seuCarro");
+  }
 }
