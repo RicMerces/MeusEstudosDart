ec86703a4361bf69c95890f567ad65a996e38847
RicMerces <ricardoliveira0703@gmail.com>
2022-02-02 19:28:19 -0300

    POO

--- dart/Modulo 04/Exemplo de Cliente/test/main.dart
+++ dart/Modulo 04/Exemplo de Cliente/test/main.dart
@@ -1,1 +1,29 @@
-main() {}
+import 'dart:io';
+import 'dart:math';
+import '../model/Cliente.dart';
+import '../model/Produto.dart';
+import '../model/Venda.dart';
+import '../model/Venda_item.dart';
+
+main() {
+  var venda = new Venda(
+      cliente: Cliente(nome: 'Neymar JR', cpf: '598-802-709-41'),
+      itens: <VendaItem>[
+        VendaItem(
+            quantidade: 10,
+            produto: Produto(
+                codigo: 1, nome: 'Caneta Bic', preco: 20.00, desconto: 0.25)),
+        VendaItem(
+            quantidade: 3,
+            produto: Produto(
+                codigo: 2, nome: 'Caderno', preco: 10.90, desconto: 0.2)),
+        VendaItem(
+            quantidade: 6,
+            produto:
+                Produto(codigo: 3, nome: 'Farofa', preco: 2.50, desconto: 0.1))
+      ]);
+
+  print("O valor total da venda é : R\$$venda.valorTotal");
+
+  print("Nome do primeiro produto é: ${venda.itens[0].produto.nome}")
+}
