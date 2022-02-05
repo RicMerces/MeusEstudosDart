3aa65a8fa488b4cc31b4f06fe345d132db6f2711
RicMerces <ricardoliveira0703@gmail.com>
2022-02-05 18:57:06 -0300

    Desafio06

--- dart/Challenges/07 - Programação Orientada a Objetos/Desafio06/Test/main.dart
+++ dart/Challenges/07 - Programação Orientada a Objetos/Desafio06/Test/main.dart
@@ -1,3 +1,34 @@
-void(){
-  
+import 'dart:io';
+import '../Model/Televisor.dart';
+
+main() {
+  informeCanal();
+}
+
+informeCanal() {
+  print("Informe um canal");
+  var canal = stdin.readLineSync()!;
+  Televisor t = new Televisor(int.parse(canal));
+
+  var acoes = 'ligar';
+  while (acoes.toLowerCase() != 'desligar') {
+    print("Informe uma ação");
+    acoes = stdin.readLineSync()!;
+    if (acoes.toLowerCase() == 'subir') {
+      t.subirCanal();
+    } else if (acoes.toLowerCase() == 'descer') {
+      t.descerCanal();
+    } else if (acoes.toLowerCase() == 'abaixar') {
+      t.diminuirVolume();
+    } else if (acoes.toLowerCase() == 'aumentar') {
+      t.aumentarVolume();
+    } else if (acoes.toLowerCase() == 'desligar') {
+      print("PowerOff");
+    } else {
+      bool x = int.parse(acoes) is int;
+      if (x == true) {
+        t.informarCanal(int.parse(acoes));
+      }
+    }
+  }
 }
