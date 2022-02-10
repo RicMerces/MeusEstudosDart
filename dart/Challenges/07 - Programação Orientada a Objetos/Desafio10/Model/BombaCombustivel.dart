9ed79acc00cdc62f83e50a435726ffb350aeb728
RicMerces <ricardoliveira0703@gmail.com>
2022-02-10 16:31:49 -0300

    Desafio10

---
+++ dart/Challenges/07 - Programação Orientada a Objetos/Desafio10/Model/BombaCombustivel.dart
@@ -0,0 +1,11 @@
+class BombaCombustivel {
+  String tipoDeCombustivel; //Nome
+  double valorDoLitro; //Reais
+  int quantidadeDeCombustivel; //Litros
+
+  abastecerPorValor() {}
+  abastecerPorLitro() {}
+  alterarValor() {}
+  alterarCombustivel() {}
+  alterarQuantidadeDoCombustivel() {}
+}
