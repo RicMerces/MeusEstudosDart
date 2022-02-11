ee509485f459d410d3bf22d8aa90bc33a2de1bba
RicMerces <ricardoliveira0703@gmail.com>
2022-02-11 17:46:03 -0300

    Metodos

--- dart/Modulo 04 - P O O/Classes-Objetos-Atributos/Classe.dart
+++ dart/Modulo 04 - P O O/Classes-Objetos-Atributos/Classe.dart
@@ -9,6 +9,17 @@ void main() {
   print(pessoa01.nome);
   print(pessoa01.idade);
 
+  pessoa01.aniversario();
+  print(pessoa01.idade);
+
+  print("Trocar Nome de ${pessoa01.nome}");
+  pessoa01.trocarNome('Roberval');
+  print("Seu novo nome = ${pessoa01.nome}");
+
+  print(pessoa01.casado);
+  pessoa01.casar();
+  print(pessoa01.casado);
+
   //Outro objeto
   Pessoa pessoa02 = Pessoa();
   pessoa02.nome = 'Wagner';
@@ -16,6 +27,11 @@ void main() {
   print("${pessoa02.idade}");
   pessoa02.casado = true;
   print(pessoa02.casado);
+
+  pessoa02.aniversario(); //idade == Null
+  pessoa02.idade = 31;
+  pessoa02.aniversario();
+  print(pessoa02.idade);
 }
 
 class Pessoa {
@@ -23,4 +39,23 @@ class Pessoa {
   String? nome; //Atributo
   int? idade;
   bool casado = false;
+
+  //Metodo
+  int? aniversario() {
+    print("Parabens $nome, de adeus aos $idade anos e de ola aos");
+    if (idade != null) {
+      idade = idade! + 1;
+    }
+
+    return idade;
+  }
+
+  void casar() {
+    print("Parabens pelo casamento $nome");
+    casado = true;
+  }
+
+  void trocarNome(String novoNome) {
+    nome = novoNome;
+  }
 }
