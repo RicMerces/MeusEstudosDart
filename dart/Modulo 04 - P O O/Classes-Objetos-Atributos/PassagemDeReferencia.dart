f34c0ebf1b3193fd48b4dacfc2f3fb26b6ff8c4c
RicMerces <ricardoliveira0703@gmail.com>
2022-02-14 15:11:54 -0300

    Passagem de referencia

---
+++ dart/Modulo 04 - P O O/Classes-Objetos-Atributos/PassagemDeReferencia.dart
@@ -0,0 +1,25 @@
+void main() {
+  Pessoa pessoa = Pessoa(nome: 'Ricardo', idade: 20);
+  print(pessoa.nome);
+  print(pessoa.idade);
+
+  Pessoa pessoa2 = pessoa;
+  pessoa.nome = 'Joaquim';
+  print(pessoa2.nome);
+  print(pessoa2.idade);
+
+  int numero = 10;
+  funcao(numero);
+  print(numero);
+}
+
+void funcao(int x) {
+  x = 60;
+}
+
+class Pessoa {
+  Pessoa({required this.nome, required this.idade});
+
+  String nome;
+  int idade;
+}
