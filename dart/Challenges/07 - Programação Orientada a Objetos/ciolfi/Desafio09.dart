b93d88e5fa03a81ac176a67ed94bc3b7352e1562
RicMerces <ricardoliveira0703@gmail.com>
2022-02-17 22:55:42 -0300

    Desafio09

--- dart/Challenges/07 - Programação Orientada a Objetos/ciolfi/Desafio09.dart
+++ dart/Challenges/07 - Programação Orientada a Objetos/ciolfi/Desafio09.dart
@@ -1,8 +1,52 @@
-class Retangulo {}
+class Retangulo {
+  Retangulo({required this.altura, required this.largura});
 
-class Ponto {
-  double x;
-  double y;
+  double altura;
+  double largura;
+
+  String pontoSelecionado(String x) {
+    String resp = '';
+    if (x == '10') {
+      Ponto inferiorDireito = new Ponto(x: largura, y: 0);
+      resp = inferiorDireito.imprimirP();
+    } else if (x == '11') {
+      Ponto superiorDireito = new Ponto(x: largura, y: altura);
+      resp = superiorDireito.imprimirP();
+    } else if (x == '00') {
+      Ponto inferiorEsquerdo = new Ponto(x: 0, y: 0);
+      resp = inferiorEsquerdo.imprimirP();
+    } else if (x == '01') {
+      Ponto superiorEsquerdo = new Ponto(x: 0, y: altura);
+      resp = superiorEsquerdo.imprimirP();
+    }
+    return resp;
+  }
+
+  String encontrarCentro() {
+    Ponto p01 = new Ponto(x: largura / 2, y: altura / 2);
+    return p01.imprimirP();
+  }
 }
 
-void main() {}
+class Ponto {
+  Ponto({required this.x, required this.y});
+  double x;
+  double y;
+
+  String imprimirP() {
+    return 'Pares Ordenados\n Eixo X = $x\n Eixo Y = $y';
+  }
+}
+
+void main() {
+  Retangulo r = new Retangulo(altura: 10, largura: 20);
+
+  print(r.pontoSelecionado('10'));
+  print(r.pontoSelecionado('11'));
+  print(r.pontoSelecionado('01'));
+  print(r.pontoSelecionado('00'));
+
+  print(r.encontrarCentro());
+  r.altura = 40;
+  print(r.encontrarCentro());
+}
