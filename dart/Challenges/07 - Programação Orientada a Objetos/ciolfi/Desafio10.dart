5de5d5bef78a4a5213823e879b40eb002831ca59
RicMerces <ricardoliveira0703@gmail.com>
2022-02-21 16:07:54 -0300

    Desafio10

--- dart/Challenges/07 - Programação Orientada a Objetos/ciolfi/Desafio10.dart
+++ dart/Challenges/07 - Programação Orientada a Objetos/ciolfi/Desafio10.dart
@@ -1,22 +1,41 @@
 void main() {}
 
 class bombaDeCombustivel {
+  bombaDeCombustivel({required this.tipoCombustivel, required this.qtdLitro});
+
   String tipoCombustivel;
-  double valorLitro;
+  double valorLitro = 0;
   double qtdLitro;
 
-  double abastecerPorValor() {}
-
-  double abastecerPorLitro() {}
-
-  double alterarValor() {}
-
-  String alterarCombustivel(String combustivel) {
-    if (combustivel.toUpperCase() == 'GASOLINA') {
-    } else if (combustivel.toUpperCase() == 'ALCOOL') {
-    } else if (combustivel.toUpperCase() == 'DIESEL') {
-    } else if (combustivel.toUpperCase() == 'ELETRICIDADE') {}
+  double abastecerPorValor(double valor) {
+    this.qtdLitro = valor / valorLitro;
+    return qtdLitro;
   }
 
-  double alterarQtdCombustivel() {}
+  double abastecerPorLitro(double litro) {
+    qtdLitro = litro;
+    double valorFinal = valorLitro * qtdLitro;
+    return valorFinal;
+  }
+
+  double alterarValor() {
+    if (tipoCombustivel.toUpperCase() == 'GASOLINA') {
+      valorLitro = 7.21;
+    } else if (tipoCombustivel.toUpperCase() == 'ALCOOL') {
+      valorLitro = 6.34;
+    } else if (tipoCombustivel.toUpperCase() == 'DIESEL') {
+      valorLitro = 5.632;
+    }
+    return valorLitro;
+  }
+
+  String alterarCombustivel(String combustivel) {
+    this.tipoCombustivel = combustivel;
+    return combustivel;
+  }
+
+  double alterarQtdCombustivel(double qtd) {
+    qtd = qtdLitro;
+    return qtd;
+  }
 }
