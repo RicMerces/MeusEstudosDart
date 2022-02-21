7600a1ff2dc224012028b19e96466867b57d0805
RicMerces <ricardoliveira0703@gmail.com>
2022-02-21 16:39:30 -0300

    Desafio10

--- dart/Challenges/07 - Programação Orientada a Objetos/ciolfi/Desafio10.dart
+++ dart/Challenges/07 - Programação Orientada a Objetos/ciolfi/Desafio10.dart
@@ -1,10 +1,21 @@
-void main() {}
+void main() {
+  bombaDeCombustivel b =
+      new bombaDeCombustivel(tipoCombustivel: 'GASOLINA', qtdLitro: 20);
+  b.alterarCombustivel('GASOLINA');
+  print(b.abastecerPorLitro(40));
+  print(b.abastecerPorLitro(10));
+
+  print(b.abastecerPorValor(72.1));
+  b.alterarCombustivel('ALCOOL');
+  print(b.abastecerPorLitro(40));
+  print(b.abastecerPorLitro(10));
+}
 
 class bombaDeCombustivel {
   bombaDeCombustivel({required this.tipoCombustivel, required this.qtdLitro});
 
   String tipoCombustivel;
-  double valorLitro = 0;
+  double valorLitro = 1;
   double qtdLitro;
 
   double abastecerPorValor(double valor) {
@@ -31,6 +42,7 @@ class bombaDeCombustivel {
 
   String alterarCombustivel(String combustivel) {
     this.tipoCombustivel = combustivel;
+    alterarValor();
     return combustivel;
   }
 
