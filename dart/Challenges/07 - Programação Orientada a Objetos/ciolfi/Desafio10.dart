c8c990c2de235bf5d64d699dc83838a40aac1a0a
RicMerces <ricardoliveira0703@gmail.com>
2022-02-21 15:38:02 -0300

    Desafio10

--- dart/Challenges/07 - Programação Orientada a Objetos/ciolfi/Desafio10.dart
+++ dart/Challenges/07 - Programação Orientada a Objetos/ciolfi/Desafio10.dart
@@ -1,1 +1,22 @@
 void main() {}
+
+class bombaDeCombustivel {
+  String tipoCombustivel;
+  double valorLitro;
+  double qtdLitro;
+
+  double abastecerPorValor() {}
+
+  double abastecerPorLitro() {}
+
+  double alterarValor() {}
+
+  String alterarCombustivel(String combustivel) {
+    if (combustivel.toUpperCase() == 'GASOLINA') {
+    } else if (combustivel.toUpperCase() == 'ALCOOL') {
+    } else if (combustivel.toUpperCase() == 'DIESEL') {
+    } else if (combustivel.toUpperCase() == 'ELETRICIDADE') {}
+  }
+
+  double alterarQtdCombustivel() {}
+}
