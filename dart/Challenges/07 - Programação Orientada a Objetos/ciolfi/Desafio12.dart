a0d699e87447e42a31d3a3d0657027c03b06f8d9
RicMerces <ricardoliveira0703@gmail.com>
2022-02-22 20:34:09 -0300

    Desafio12

--- dart/Challenges/07 - Programação Orientada a Objetos/ciolfi/Desafio12.dart
+++ dart/Challenges/07 - Programação Orientada a Objetos/ciolfi/Desafio12.dart
@@ -1,24 +1,47 @@
-void main() {}
+void main() {
+  contaInvestimento c1 = contaInvestimento(nome: 'Ricardo', numeroDaConta: 59881931);
+
+  c1.
+
+}
 
 class contaInvestimento {
-  contaInvestimento({required nome, required numeroDaConta});
+  contaInvestimento({required this.nome, required this.numeroDaConta});
 
   String nome;
   int numeroDaConta;
   double _saldoAtual = 1000;
-  double juros = 10;
+  double juros = 0.1;
 
   double get saldoAtual {
     return _saldoAtual;
   }
 
-  void set saldoAtual(saldo) {
+  void set saldoAtual(double saldo) {
     this._saldoAtual = saldo;
   }
 
-  depositar() {}
+  double depositar(double valor) {
+    print("Deposito realizado !");
+    saldoAtual += valor;
+    return saldoAtual;
+  }
 
-  sacar() {}
+  double sacar(double valor) {
+    print("Saque realizado !");
+    saldoAtual -= valor;
+    return saldoAtual;
+  }
 
-  adicionarJuros() {}
+  double alterarValorJuros(double juros){
+    this.juros = juros;
+    return this.juros;
+  }
+
+
+  double adicionarJuros() {
+    print("Juros adicionados !");
+    saldoAtual -= saldoAtual * juros;
+    return saldoAtual;
+  }
 }
