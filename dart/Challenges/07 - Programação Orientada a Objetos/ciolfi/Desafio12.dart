8970efaa3c242deccd58a874d2f889b22114f29c
RicMerces <ricardoliveira0703@gmail.com>
2022-02-22 20:04:50 -0300

    Desafio12

--- dart/Challenges/07 - Programação Orientada a Objetos/ciolfi/Desafio12.dart
+++ dart/Challenges/07 - Programação Orientada a Objetos/ciolfi/Desafio12.dart
@@ -1,3 +1,24 @@
 void main() {}
 
-class contaInvestimento {}
+class contaInvestimento {
+  contaInvestimento({required nome, required numeroDaConta});
+
+  String nome;
+  int numeroDaConta;
+  double _saldoAtual = 1000;
+  double juros = 10;
+
+  double get saldoAtual {
+    return _saldoAtual;
+  }
+
+  void set saldoAtual(saldo) {
+    this._saldoAtual = saldo;
+  }
+
+  depositar() {}
+
+  sacar() {}
+
+  adicionarJuros() {}
+}
