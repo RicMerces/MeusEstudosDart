3db92eca2f1744c91f1b983a1626604574644765
RicMerces <ricardoliveira0703@gmail.com>
2022-02-23 13:16:10 -0300

    Desafio14

---
+++ dart/Challenges/07 - Programação Orientada a Objetos/ciolfi/Desafio14.dart
@@ -0,0 +1,31 @@
+import 'dart:io';
+
+void main() {
+  print("Informe seu nome");
+  var n = stdin.readLineSync()!;
+
+  print("Informe seu salario");
+  var sal = stdin.readLineSync()!;
+  double s = double.parse(sal);
+
+  Funcionario f = new Funcionario(nome: n, salario: s);
+
+  f.adicionarSalario(20);
+  print(f);
+}
+
+class Funcionario {
+  Funcionario({required this.nome, required this.salario});
+
+  String nome;
+  double salario;
+
+  adicionarSalario(double adicionar) {
+    salario += adicionar;
+    return salario;
+  }
+
+  String toString() {
+    return '\n ------------------ \nNome do Funcionario : $nome\nSalario R\$ $salario \n------------------';
+  }
+}
