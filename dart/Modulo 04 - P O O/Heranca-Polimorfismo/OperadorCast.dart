bbe10358d88d2bcec3e6f0d653dcb7c8cf37e317
RicMerces <ricardoliveira0703@gmail.com>
2022-02-23 17:29:46 -0300

    Operador Cast

---
+++ dart/Modulo 04 - P O O/Heranca-Polimorfismo/OperadorCast.dart
@@ -0,0 +1,56 @@
+void main() {
+  Cachorro c = new Cachorro('Nina', 15);
+  c.comer();
+  c.dormir();
+
+  Gato g = new Gato('Toquinho', 1);
+  g.miar();
+  g.dormir();
+  g.comer();
+
+  Animal animal1 = funcao();
+
+  Gato g2 = funcao() as Gato;
+
+  print(animal1);
+  print(g2);
+}
+
+class Cachorro extends Animal {
+  Cachorro(String nome, int idade) : super(nome, idade);
+
+  void latir() {
+    print("AUAUAUAUAU");
+  }
+}
+
+class Gato extends Animal {
+  Gato(String nome, int idade) : super(nome, idade);
+
+  void miar() {
+    print("Miaaaauuuu");
+  }
+}
+
+Animal funcao() {
+  return Gato('Kyra', 2);
+}
+
+class Animal {
+  Animal(this.nome, this.idade);
+
+  String nome;
+  int idade;
+
+  void comer() {
+    print("$nome esta comendo");
+  }
+
+  void dormir() {
+    print("zZzZZZZZzzz");
+  }
+
+  String toString() {
+    return 'Nome do bixinho : $nome\nIdade : $idade';
+  }
+}
