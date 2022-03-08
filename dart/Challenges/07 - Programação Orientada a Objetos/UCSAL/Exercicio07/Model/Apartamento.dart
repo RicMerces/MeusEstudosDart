927355f178b4922fab0b16a688c843125d3869cf
RicMerces <ricardoliveira0703@gmail.com>
2022-03-08 19:38:05 -0300

    Desafio07

---
+++ dart/Challenges/07 - Programação Orientada a Objetos/UCSAL/Exercicio07/Model/Apartamento.dart
@@ -0,0 +1,8 @@
+import 'Imovel.dart';
+
+class Apartamento extends Imovel {
+  Apartamento
+
+  double? fracaoIdeal;
+  double? areaPrivada;
+}
---
+++ dart/Challenges/07 - Programação Orientada a Objetos/UCSAL/Exercicio07/Model/Imovel.dart
@@ -0,0 +1,12 @@
+class Imovel {
+  Imovel(
+      {required this.codigo,
+      required this.endereco,
+      required this.bairro,
+      required this.valor});
+
+  int? codigo;
+  String? endereco;
+  String? bairro;
+  double? valor;
+}
