0c34e3213f7fd3cab0aebc1dc383a7210c86774b
RicMerces <ricardoliveira0703@gmail.com>
2022-03-11 11:29:09 -0300

    Desafio09

--- dart/Challenges/07 - Programação Orientada a Objetos/UCSAL/Exercicio08/Model/Professor.dart
+++ dart/Challenges/07 - Programação Orientada a Objetos/UCSAL/Exercicio08/Model/Professor.dart
@@ -4,10 +4,4 @@ class Professor extends Cadastro {
   Professor(String nome, int telefone, String lugar, this.titulo)
       : super(nome: nome, telefone: telefone, nacionalidade: lugar);
   String? titulo;
-
-  String? validarTitulo() {
-    if (titulo.toUpperCase() is titulacao) {}
-  }
 }
-
-enum titulacao { ESPECIALISTA, MESTRE, DOUTOR }
